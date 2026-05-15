dda4a90e63d3499d40c1843a95d40d2dfed9b409
OmKindOfficial <supportomkind@gmail.com>
2026-05-15 15:36:42 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-// src/main.jsx
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
